34632423313eccfd0d972ce94378ffe8cb299078
Prakash <luckyycool@yahoo.com>
2022-03-26 09:59:48 +0530

    removed dummy setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <div className='title'>Portfolio Management</div>
+      </div>
+    
+      <div className='App-body'>
+        <div className='leftPanel'>
+          <div>Home</div>
+          <div>Order</div>
+          <div>Order History</div>
+        </div>
+        <div className='rightPanel'></div>
+      </div>
     </div>
   );
 }
